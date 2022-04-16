9cdce3071b0410ca1fa64347ab9bfd2ea6ff7857
Tanya <yallina.ta@yandex.ru>
2022-04-16 13:08:36 +1000

    Lesson7
    Lesson7

---
+++ OOP_Account/Lesson7/ACode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7
+{
+    /// <summary>
+    /// Класс шифрует строку посредством сдвига каждого символа на одну позицию 
+    /// </summary>
+    internal class ACode : ICode
+    {
+        public string Encode(string text)
+        {
+            char[] chars = text.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                chars[i]++;
+            }
+            return new string(chars); ;
+        }
+
+        public string Decode(string text)
+        {
+            return Encode(text);
+        }
+    }
+}
